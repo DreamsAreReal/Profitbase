ab0ffbc5934713127050f4daa55e349984019d25
ProgWidness <46569007+ProgWidness@users.noreply.github.com>
2021-04-26 08:29:42 -0400

    Made class for use wrapper in other classes

---
+++ Profitbase.Wrapper/Api.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Profitbase.Wrapper
+{
+    public class Api
+    {
+        public void GetAll(string login, string password, IWebProxy proxy = null)
+        {
+            return;
+        }
+    }
+}
