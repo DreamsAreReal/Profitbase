e0792c576a5ba27681c61421655af59e5a5464d7
ProgWidness <46569007+ProgWidness@users.noreply.github.com>
2021-04-28 06:22:39 -0400

    Create request for get projects

---
+++ Profitbase.Wrapper/Requests/ProjectsRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Profitbase.Wrapper.Models;
+
+namespace Profitbase.Wrapper.Requests
+{
+    class ProjectsRequest
+    {
+        private readonly IClient _client;
+        public ProjectsRequest(IClient client)
+        {
+            _client = client;
+        }
+
+
+        public async Task Execute(ApiDataModel apiData)
+        {
+            var data 
+        }
+    }
+}
